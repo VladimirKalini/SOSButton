c4e0041518e5e7c63bd9866a92cc81ca69309650
VlsdimirKalinin <rainzapp@gmail.com>
2025-05-23 12:13:57 +0300

    Fix '/*'

--- src/server.js
+++ src/server.js
@@ -5,54 +5,46 @@ const { Server } = require('socket.io');
 const path = require('path');
 const authRoutes = require('./routes/auth');
 
-
 const app = express();
 const httpServer = http.createServer(app);
 
-// 1) JSON-парсер для API
+// 1) Парсинг JSON для API
 app.use(express.json());
 
-// 2) Подключаем API-маршруты
+// 2) Маршруты API (регистрация, логин и т.д.)
 app.use('/api', authRoutes);
 
-// 3) Статика React и SPA-роутинг
+// 3) Статика React
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (req, res) => {
-  // Любой путь, не начинающийся с /api, отдадим index.html
+
+// 4) SPA-роутинг: без параметров внутри path-to-regexp
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// 4) Socket.IO для видеопотоков SOS
+// 5) Socket.IO для видеопотоков SOS
 const io = new Server(httpServer, {
   cors: { origin: '*' }
 });
 
-// JWT-middleware (пока заглушка)
 io.use((socket, next) => {
-  // const token = socket.handshake.auth.token;
-  // TODO: верифицировать токен, присвоить socket.data.role
+  // TODO: проверка socket.handshake.auth.token
   next();
 });
 
 io.on('connection', socket => {
-  // Модератор заходит в комнату
-  socket.on('join-room', room => {
-    socket.join(room);
-  });
+  socket.on('join-room', room => socket.join(room));
 
-  // Пользователь нажал SOS
   socket.on('sos-offer', ({ offer, latitude, longitude, phone, id }) => {
-    // Рассылаем всем в комнате moderators
     socket.to('moderators').emit('incoming-sos', { offer, latitude, longitude, phone, id });
   });
 
-  // ICE-кандидаты от клиента
   socket.on('ice-candidate', ({ candidate, id }) => {
     socket.to(id).emit('ice-candidate', candidate);
   });
 });
 
-// 5) Запуск сервера
+// 6) Запуск сервера
 const PORT = process.env.PORT || 4000;
 httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
