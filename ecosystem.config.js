1e9ed37b3a36434699b61f9e23b4bef5dddd10ec
VlsdimirKalinin <rainzapp@gmail.com>
2025-06-05 17:21:58 +0300

    Перенос на новую ВПС изменение путей и почт

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,24 +3,24 @@ module.exports = {
     {
       name: 'sos-backend',
       script: './server.js',
-      cwd: '/var/www/SOSButton',   
+      cwd: '/var/www/sos-app',   
       watch: true,                
       env: {
         NODE_ENV: 'production',
         PORT: 4000,
-        MONGO_URI: 'mongodb://localhost:27017/SOSButton'
+        MONGO_URI: 'mongodb://localhost:27017/sos-app'
       }
     },
     {
       name: 'video-cleanup',
       script: './src/scripts/cleanupVideos.js',
-      cwd: '/var/www/SOSButton',
+      cwd: '/var/www/sos-app',
       watch: false,
       cron_restart: '0 0 * * *', // Запуск каждый день в полночь
       autorestart: false,
       env: {
         NODE_ENV: 'production',
-        MONGO_URI: 'mongodb://localhost:27017/SOSButton'
+        MONGO_URI: 'mongodb://localhost:27017/sos-app'
       }
     }
   ]
